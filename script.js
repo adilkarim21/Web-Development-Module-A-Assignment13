cc501b76414d7f56c7380c0c8d4722edd1150414
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-09-09 00:41:18 +0500

    add files

---
+++ script.js
@@ -0,0 +1,188 @@
+// Chap (1-3) Home Assignment:
+
+// Chapter 1 (Alerts)
+
+
+// Q1:
+// 1.
+// alert("Adil")
+// 2.
+// alert("Khosa")
+// 3.
+// alert("muhammadadilmemon79@gmail.com")
+// 4.
+// alert("03362525472")
+// 5.
+// alert("12345678")
+
+// Q2:
+// statement
+// alert "You're learning Javascript!";
+// correct statement
+// alert("You're learning Javascript!");
+
+// Q3:
+// statement message
+// alert("Page is under construction")
+
+
+
+
+// Chapter 2 (Variables for string)
+
+// Q1:
+// var firstName
+
+// Q2:
+// var num
+// num = nothing
+
+// Q3:
+// var teamName = "TeamProfessor"
+// alert(teamName);
+
+// Q4:
+// var bestMan = "Charlie";
+// bestName = "Batman"
+
+
+
+// Chapter 3 (Variables for numbers)
+
+// Q1:
+// var caseQty
+
+// Q2:
+// caseQty = 144;
+
+// Q3:
+// var num = 9;
+
+// Q4:
+// var sum
+// sum = 2 + 3
+
+// Q5:
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal)
+
+// Q6:
+// var num = 5;
+// num += 3
+// alert(num)
+
+// ***********************************************************************
+
+
+
+// Class Task:
+
+// Chap 1 (Alert)
+
+
+// Q1:
+// alert("Jese aae ho wese chale jao")
+
+// Q2:
+// alert("Error! Please enter a valid password.")
+
+// Q3:
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// Q4:
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Q5:
+// console.log("Hello... I can run JS through my web browser's console")
+
+// Q6:
+// alert("KuchBhi")
+
+
+
+// Chap 2 (VARIABLES FOR STRINGS)
+
+
+// Q1:
+// var userName
+
+// Q2:
+// var myName = AdilKhosa
+
+// Q3:
+// a)
+// var message
+// b)
+// message = "Hello World"
+// c)
+// alert("message")
+
+// Q4:
+// var name = "Jhone Doe"
+// alert("name")
+// var age = "15 years old"
+// alert("age")
+// var qualification = "Certified Mobile Application Development"
+// alert("qualification")
+
+// Q5:
+// var text = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(text)
+
+// Q6:
+// var email = "example@gmail.com"
+// alert("My email address is " + email)
+
+// Q7:
+// var book = "A smarter way to learn Javascript"
+// alert("I am trying to learn from the Book " + book)
+
+// Q8:
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// Q9:
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// Chap 3 (VARIABLES FOR NUMBERS)
+
+
+// Q1:
+// var age = 17
+// alert("I am " + age + " years old")
+
+// Q2:
+// var N = 4
+// alert("You have visited this site " + N + " times")
+
+// Q3:
+// var birthYear = 2007
+// document.write("My birth year is " + birthYear)
+
+// Q4:
+// var visitorName = "Jhon Doe"
+// var quantity = 3
+// var productTitle = "T-shirt(m)"
+// document.write(visitorName + " ordered " + quantity + productTitle + " on XYZ CLOTHING STORE")
+
+
+
+// Chap 4 (VARIABLE NAMES: LEGAL & ILLEGAL)
+
+
+// 1.
+// var num, num2, num3
+
+// 2.
+// Legal Variable Names:
+// var num , num1 , $num , num$ , numbers  
+// Illegal Variable Names:
+// var 1num , #num , &num ,%num , @num 
+
+// 3.
+// document.write(
+//     "<h1>Rules for naming JS variables </h1><br> <br><p>Variable names can only contain , numbers , $ and _ . For example : $my_1stVariable</p><p>Variables must begin with a letter, $ or _ . For example : $name, _name, name</p><p>Variable names are case sensitive</p><p>Variable names should not be JS Keywords</p>"
+// )
